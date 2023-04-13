b33dadb4911fc555db583e6c8575c3035ed79507
Thalia Rodriguez <thalia03@ad.unc.edu>
2023-04-12 23:47:31 -0400

    889

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const port = args.port || 3000;
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 
-fs.readFile('./public/index.html', (err, data) => {
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if(err) {
         console.error(err)
         return;
