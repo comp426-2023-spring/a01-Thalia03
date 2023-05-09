7d913c240f60fc205a80456d6d6951d05308d8b2
Thalia Rodriguez <thalia03@ad.unc.edu>
2023-05-09 17:56:58 -0400

    klhl

--- server.js
+++ server.js
@@ -52,8 +52,9 @@ const server = http.createServer((req, res) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
     
-server.listen(port);
-console.log("Server listening on port" + port);
+server.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
 
     
 
