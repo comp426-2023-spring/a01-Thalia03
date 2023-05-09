ecc1f255e33010a27252da7569d9a28ea5cf8efd
Thalia Rodriguez <thalia03@ad.unc.edu>
2023-05-09 17:23:46 -0400

    ?

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 // Require http module
-const http = require('http');
+import { createServer } from 'http';
 
 // Require fs module
-const fs = require('fs');
+import { readFile } from 'fs';
 
 // Require minimist module (make sure you install this one via npm).
-const minimist = require('minimist');
+import minimist from 'minimist';
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = minimist(process.argv.slice(2));
-
+var file;
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || process.env.PORT || 3000;
+const port = args['port'] || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 
-fs.readFile("./public/index.html", "utf8", (err, dt) => {
-
+readFile("./public/index.html", "utf8", (err, dt) => {
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
@@ -27,8 +26,8 @@ fs.readFile("./public/index.html", "utf8", (err, dt) => {
         console.error(err);
         return;
     }
-
-})
+    file = dt;
+});
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -37,15 +36,11 @@ fs.readFile("./public/index.html", "utf8", (err, dt) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-const server = http.createServer((req, res) => {
-    fs.readFile("./public/index.html", "utf8", (err, dt) => {
-        if(err) {
-            console.error(err); return;
-        }
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(dt);
-    });
-});
+const server = createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/html')
+    res.end(file)
+})
 
 
 
@@ -53,11 +48,10 @@ const server = http.createServer((req, res) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
     
-server.listen(port);
-console.log(`Server listening on port: ${port}`);
-    
-    
+server.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
 
-
+    
 
 // That's it! You're all done!
